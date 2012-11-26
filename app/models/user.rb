1052357d1e417b0f4e43c208d670c146c2b862dc
Xu-Zonne <xuzonne@outlook.com>
2012-11-26 23:15:48 +0800

    Added gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,5 +26,11 @@ has_many :statuses
 		first_name + " " + last_name
 	end 
 
+ def gravatar_url
+  stripped_email = email.strip 
+  downcased_email = stripped_email.downcase
+  hash = Digest::MD5.hexdigest(downcased_email)
 
+  "http://gravatar.com/avatar/#{hash}"
+  end
 end
